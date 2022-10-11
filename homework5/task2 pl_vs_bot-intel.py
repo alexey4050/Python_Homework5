2257d32efd22ecbe84f44279a21c1a8c0a4b219a
alexey <alexey4050@mail.ru>
2022-10-11 22:21:05 +0300

    Конфеты (человек и бот с интеллектом

---
+++ homework5/task2 pl_vs_bot-intel.py
@@ -0,0 +1,58 @@
+# Создайте программу для игры с конфетами человек против человека.
+
+# Условие задачи: На столе лежит 2021 конфета.
+# Играют два игрока делая ход друг после друга.
+# Первый ход определяется жеребьёвкой. 
+# За один ход можно забрать не более чем 28 конфет. 
+# Все конфеты оппонента достаются сделавшему последний ход.
+# Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?
+
+# player_vs_bot + "интеллект"
+
+from random import randint
+
+def input_dat(name):
+    x = int(input(f"{name}, введите количество конфет, которые возьмете от 1 до 28: "))
+    while x < 1 or x > 29:
+        x = int(input(f"{name}, введите корректное количество конфет: "))
+    return x
+    
+def p_print(name, k, counter, value):
+    print(f"Ходил {name}, он взял {k}, теперь у него {counter}. Осталось на столе {value} конфет. \n")
+
+def bot_calc(value):
+    k = randint(1, 29)
+    while value-k <= 28 and value > 29:
+        k = randint(1, 29)
+    return k
+
+player1 = input("Введите имя первого игрока: ")
+player2 = "Bot"
+value = int(input("Введите количетсво конфет на столе: "))
+who_the_first = randint(0, 2)
+if who_the_first:
+    print(f"Первым ходит {player1}")
+else:
+    print(f"Первым ходит {player2}")
+    
+counter1 = 0
+counter2 = 0
+
+while value > 28:
+    if who_the_first:
+        k = input_dat(player1)
+        counter1 += k
+        value -= k
+        who_the_first = False
+        p_print(player1, k, counter1, value)
+    else:
+        k = bot_calc(value)
+        counter2 += k
+        value -= k
+        who_the_first = True
+        p_print(player1, k, counter2, value)
+        
+if who_the_first:
+    print(f"Выйграл {player1} !!!")
+else:
+    print(f"Выйграл {player2} !!!")
